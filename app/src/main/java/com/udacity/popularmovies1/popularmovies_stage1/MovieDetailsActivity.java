10c7ff2fb2293ff059d8c19e20dcbd73b8f2f04b
Federico Creti <federico.creti87@outlook.it>
2018-02-21 11:21:16 +0100

    Exercise completed, code review 4

--- app/src/main/java/com/udacity/popularmovies1/popularmovies_stage1/MovieDetailsActivity.java
+++ app/src/main/java/com/udacity/popularmovies1/popularmovies_stage1/MovieDetailsActivity.java
@@ -1,6 +1,7 @@
 package com.udacity.popularmovies1.popularmovies_stage1;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -23,6 +24,13 @@ public class MovieDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+
+        ActionBar actionBar = this.getSupportActionBar();
+
+        // Set the action bar back button to look like an up button
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
         //Retrieve the UI components
         poster = findViewById(R.id.movie_poster);
